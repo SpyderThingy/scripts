d733f803ac4634e5703492b94eb4a29a526b22a9
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-10-23 10:43:56 +0100

    add comment

--- even.js
+++ even.js
@@ -1,5 +1,5 @@
 var number = window.prompt("Gimme a number.")
-if (number % 2 == 0) {
+if (number % 2 == 0) { // the percent sign sends the remainder back
     alert("That's an even number or smth idk i forgor mdgkjbhdsmbj")
 } else if (number % 2 == 1) {
     alert("you are one silly oddball")
