416b68f9690c6502805664d73a8c8c0676b061c5
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-10-25 13:49:01 +0100

    Fix previous commit

--- .js
+++ .js
@@ -1,6 +1,6 @@
 var x = 0
 var y = 0
 while (x!=10) {
-    x = window.prompt("Enter a number larger than 10.")
+    x = window.prompt("Enter a number equal to 10.")
     y++
 } alert("The amount of tries needed for this to succeed were: " + y)
