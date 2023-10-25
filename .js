2f3904417e41644ee1bfd0c5ebf0d2513bcb75ab
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-10-25 13:33:33 +0100

    Fix grammatical error

--- .js
+++ .js
@@ -3,4 +3,9 @@ var y = 0
 while (x<=10) {
     x = window.prompt("Enter a number larger than 10.")
     y++
-} alert("It took you " + y + " attempts.")
+}
+if (y = 1) {
+    alert("It took you 1 attempt. Great job!")
+} else {
+    alert("It took you " + y + " attempts.")
+}
