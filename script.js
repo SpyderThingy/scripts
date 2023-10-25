7ed14cea34e0b66f7cc73d029b38cfc64ed5df64
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-10-25 13:17:51 +0100

    Add comments

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ if (startScript == 1) {
     number1 = window.prompt("What number do you wish to multiply?") // he multiplies
     var number2
     number2 = window.prompt("What are you multiplying it by?") // he goin ham
-    alert("The answer is " + number1 * number2 + ".")
+    alert("The answer is " + number1 * number2 + ".") //answer :woohoo:
 } else if (startScript == 2) {
     var colour
     colour = window.prompt("What's your favourite colour?")
-    document.write("Your favourite colour is " + colour + ".")
+    document.write("Your favourite colour is " + colour + ".") //literally obvious
 } else if (startScript == 3) {
     var firstName
     firstName = window.prompt("What's your first name?")
     var lastName
     lastName = window.prompt("What's your last name?")
-    document.write(lastName + " " + firstName)
+    document.write(lastName + " " + firstName) //also obvious
 } else if (startScript == 4) {
-    var prompts
+    var prompts //this is a test. i used it as my first time with if/else statements after 4-5 years
     alert("Hello! This is a text box. I think it's kinda neat.")
     prompts = window.prompt("is this cool?")
 if (prompts.toLowerCase == "yes") {
@@ -32,5 +32,5 @@ if (prompts.toLowerCase == "yes") {
     hourlyRate = window.prompt("Input your hourly rate.")
     var hoursWorked
     hoursWorked = window.prompt("Input how long you work.")
-    alert("Your weekly pay is " + "£" + hourlyRate * hoursWorked + " per week.")        
+    alert("Your weekly pay is " + "£" + hourlyRate * hoursWorked + " per week.") //the multiplier reworded       
 }
