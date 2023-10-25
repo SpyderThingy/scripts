e0d02d0f0458db905bf9b24e9066e4d6ae8f519e
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-10-25 13:35:53 +0100

    Last commit bugged

--- .js
+++ .js
@@ -3,9 +3,4 @@ var y = 0
 while (x<=10) {
     x = window.prompt("Enter a number larger than 10.")
     y++
-}
-if (y = 1) {
-    alert("It took you 1 attempt. Great job!")
-} else {
-    alert("It took you " + y + " attempts.")
-}
+} alert("The amount of tries needed for this to succeed were: " + y)
