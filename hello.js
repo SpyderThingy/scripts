af15c1db80e57f47e37ab0b7128b187bfeba4f82
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-13 11:01:43 +0000

    haha funny code

--- hello.js
+++ hello.js
@@ -4,4 +4,10 @@
 document.write ("Hello World!")
 var mood
 mood = window.prompt("How are you?")
-document.write("Why are you feeling" + mood + "?")
+var reason = window.prompt("Why are you feeling" + mood + "?")
+if (mood.toLowerCase == "sad" || mood.toLowerCase == "angry") {
+    alert("damn, sucks to suck HAHAHAHAHAHA")
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+} else if (mood.toLowerCase == "happy") {
+    alert("YAY YIPPEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+}
