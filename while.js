3bd6391d4f6cf628080a94e5620a3c2a3d929b81
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-15 13:27:58 +0000

    Test if while.js works correctly

--- while.js
+++ while.js
@@ -1,6 +1,7 @@
 var random = Math.floor(Math.random()*10)
 var guess = window.prompt("Enter your guess to this lucky number!")
 var times = 1
+console.log(random)
 while (guess > random) {
     guess = window.prompt("Wrong! The answer is lower.")
     times = times++
