2cfe03d89d348c8163f2cbb63e10d4c3ce6c1338
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-15 13:29:24 +0000

    Fix while.js

--- while.js
+++ while.js
@@ -1,6 +1,6 @@
-var random = Math.floor(Math.random()*10)
-var guess = window.prompt("Enter your guess to this lucky number!")
-var times = 1
+random = Math.floor(Math.random()*10)
+guess = window.prompt("Enter your guess to this lucky number!")
+times = 1
 console.log(random)
 while (guess > random) {
     guess = window.prompt("Wrong! The answer is lower.")
