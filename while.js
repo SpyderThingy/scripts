e0511013365f4872cbad78481ba6ac6cd0a0661e
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-15 14:01:57 +0000

    Remove console timer

--- while.js
+++ while.js
@@ -1,6 +1,6 @@
 var random = Math.floor(Math.random()*10) 
-console.log("Shh! The number is " + random + ". Don't tell anyone!") 
-guess = window.prompt("Guess my lucky number!") 
+console.log("Shh! The number is " + random + ". Don't tell anyone!")
+guess = window.prompt("Guess my lucky number!")
 times = 1
 while (guess!=random) {
     if (guess < random) {
@@ -11,5 +11,6 @@ while (guess!=random) {
         console.error("You suffer from a severe case of skill issue.")
     }
     times++ 
-} alert("YIPPEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!")
+}
+alert("YIPPEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!")
 console.log("The amount of tries needed for this to succeed were: " + times)
