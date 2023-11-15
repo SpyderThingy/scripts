f119e172912ee3280627508c7cdc95bb29160a11
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-15 13:42:18 +0000

    Log attempts and alert a happiness

--- while.js
+++ while.js
@@ -1,8 +1,12 @@
 var random = Math.floor(Math.random()*10) 
 console.log("Shh! The number is " + random + ". Don't tell anyone!") 
-guess = 0 
-times = 0 
-while (guess!=random) { 
-    guess = window.prompt("Guess my lucky number!") 
+guess = window.prompt("Guess my lucky number!") 
+times = 1
+while (guess!=random) {
+    if (guess < random) {
+        guess = window.prompt("Wrong! The answer is higher!") 
+    } else {
+        guess = window.prompt("Wrong! The answer is lower!") 
+    }
     times++ 
-} alert("The amount of tries needed for this to succeed were: " + times) 
+} alert("YIPPEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!") && console.log("The amount of tries needed for this to succeed were: " + times)
