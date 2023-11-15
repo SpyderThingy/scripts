163071c6f0cb9bb02caa2bd3e9f0793959f92371
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-15 13:49:54 +0000

    Move error

--- while.js
+++ while.js
@@ -5,10 +5,11 @@ times = 1
 while (guess!=random) {
     if (guess < random) {
         guess = window.prompt("Wrong! The answer is higher!")
+        console.error("You suffer from a severe case of skill issue.")
     } else {
-        guess = window.prompt("Wrong! The answer is lower!") 
+        guess = window.prompt("Wrong! The answer is lower!")
+        console.error("You suffer from a severe case of skill issue.")
     }
     times++ 
-    console.error("You suffer from a severe case of skill issue.")
 } alert("YIPPEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!")
 console.log("The amount of tries needed for this to succeed were: " + times)
