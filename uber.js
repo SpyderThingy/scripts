6c2a18f75b2de9e504a540654db8e4b66df823ca
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-22 09:57:44 +0000

    add cosmetic error for shits and giggles

--- uber.js
+++ uber.js
@@ -3,6 +3,7 @@ distance = parseFloat(window.prompt("How far is your journey?"))
 time = parseFloat(window.prompt("How long will it take?")*0.1)
 fare = time+distance+base
 if (fare < 2.5) {
+    console.error("PRICE TOO LOW; SWITCHING TO MINIMUM FARE, £2.50")
     alert("Your fare is £2.50")
 } else {
     alert("Your fare is £" + fare + "0.")
