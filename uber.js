85967ded5c7530a9db82d18d9cb453391bf9c286
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-22 09:50:29 +0000

    uber

--- uber.js
+++ uber.js
@@ -1,3 +1,7 @@
 fare = 1.10 * parseInt(window.prompt("Approximately how long is your journey in miles?"))
-fare = fare * parseFloat(window.prompt("How long will your journey take?")/10)
-alert("Your fare is £" + fare + "0.")
+fare = fare * window.prompt("How long will your journey take?")/10
+if (fare < 2.5) {
+    alert("Your fare is £2.50")
+} else {
+    alert("Your fare is £" + fare + "0.")
+}
