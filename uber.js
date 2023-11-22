bf0f6a566b2fec0bd2819607f0a0678313754cf3
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-22 09:44:45 +0000

    Add uber.js

---
+++ uber.js
@@ -0,0 +1,3 @@
+fare = 1.10 * parseInt(window.prompt("Approximately how long is your journey in miles?"))
+fare = fare * parseFloat(window.prompt("How long will your journey take?")/10)
+alert("Your fare is £" + fare + "0.")
