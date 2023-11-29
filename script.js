2e4f1483aae753a2dbceb94a66b183b74358739e
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-29 13:26:04 +0000

    Add missing number

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("The scripts are: the multiplier, the one that states your favourite colour, the name swapper, the little test I made, the weekly pay calculator, the address formatter, the Uber® fare calculator and my while loop test.")
-startScript = window.prompt("Which script should be started? Answer with 1 to  respectively. Check the console for the script list.")
+startScript = window.prompt("Which script should be started? Answer with 1 to 9 respectively. Check the console for the script list.")
 if (startScript == 1) {
     number1 = window.prompt("What number do you wish to multiply?") // he multiplies
     number2 = window.prompt("What are you multiplying it by?") // he goin ham
