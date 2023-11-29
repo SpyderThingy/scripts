e19774d99bbc16d580fea404198ea8f1e5fa70c7
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-11-29 13:36:48 +0000

    Explain usage of modulus

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ if (startScript == 1) {
     console.log("The amount of tries needed for this to succeed were: " + times)
 } else if (startScript == 9) {
     number = window.prompt("Gimme a number.")
-    if (number % 2 == 0) { // the percent sign sends the remainder back
+    if (number % 2 == 0) { // the percent sign sends the remainder back with modulus
         alert("That's an even number or smth idk i forgor mdgkjbhdsmbj")
     } else if (number % 2 == 1) {
         alert("you are one silly oddball")
