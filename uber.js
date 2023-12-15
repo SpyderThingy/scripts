5976e10c91253fec3c6d6c71a1c12fc3ab4302c3
Benyamin Karolia <22BKarolia@heckgrammar.co.uk>
2023-12-15 10:15:22 +0000

    Replace files with newer files

--- uber.js
+++ uber.js
@@ -5,6 +5,8 @@ fare = time+distance+base
 if (fare < 2.5) {
     console.error("PRICE TOO LOW; SWITCHING TO MINIMUM FARE, £2.50")
     alert("Your fare is £2.50")
-} else {
+} else if (fare > 2.5){
     alert("Your fare is £" + fare + "0.")
+} else if (fare = NaN) {
+    alert("alright maybe focus your tab you smart guy")
 }
