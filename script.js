aeb6ee41d7e2bab53620f96549374d8d6d9a3101
tungstengmd <gostdud@gmail.com>
2023-12-19 10:14:07 +0000

    🔥 Remove redundant if in else statement, case 9

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ switch (startScript) {
         number = window.prompt("Gimme a number.")
         if (number % 2 == 0) { // the percent sign sends the remainder back with modulus
             alert("That's an even number or smth idk i forgor mdgkjbhdsmbj")
-        } else if (number % 2 == 1) {
+        } else {
             alert("you are one silly oddball")
         }
         break
